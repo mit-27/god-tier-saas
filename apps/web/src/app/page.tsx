82f85fa809c75715cb1b8c5b0e7f6fee1c4a98c0
mit-27 <meetsuthar64@gmail.com>
2024-09-18 16:33:36 -0600

    🎉 Add NextJS and NestJS

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Button>Hello</Button>
+
+    </div>
+  );
+}
