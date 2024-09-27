05494ce012613b5c24c063121ca14a2bd6af8b76
mit-27 <meetsuthar64@gmail.com>
2024-09-27 00:36:36 -0600

    ✨ Add posthog

--- apps/server/src/core/core.module.ts
+++ apps/server/src/core/core.module.ts
@@ -1,16 +1,17 @@
-import { Module, Global } from '@nestjs/common';
+import { Module, Global, Post } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
 import { DrizzleModule } from './drizzle/drizzle.module';
+import { PostHogService } from './posthog/posthog.service';
 
 @Global()
 @Module({
     imports: [
         AuthModule,
-        DrizzleModule
+        DrizzleModule,
     ],
     controllers: [],
-    providers: [AuthService],
-    exports: [DrizzleModule, AuthModule, AuthService],
+    providers: [AuthService, PostHogService],
+    exports: [DrizzleModule, AuthModule, AuthService, PostHogService],
 })
 export class CoreSharedModule { }
--- apps/server/src/post/post.service.ts
+++ apps/server/src/post/post.service.ts
@@ -3,39 +3,45 @@ import { DRIZZLE } from '@/core/drizzle/drizzle.module';
 import { post, DrizzleDB, NewPost } from '@template/shared';
 import { eq } from 'drizzle-orm';
 import { PinoLogger } from 'nestjs-pino';
+import { PostHogService } from '@/core/posthog/posthog.service';
 
 @Injectable()
 export class PostService {
 
-    constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB, private readonly logger: PinoLogger) {
+    constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB, private readonly logger: PinoLogger, private readonly postHogService: PostHogService) {
         this.logger.setContext(PostService.name);
     }
 
     async getPosts() {
         this.logger.info('Fetching posts');
+        this.postHogService.captureEvent('posts', 'fetched');
         return this.db.select().from(post);
     }
 
     async addPost(newPost: NewPost) {
         this.logger.info('Adding post');
+        this.postHogService.captureEvent('posts', 'added');
         const allPosts = await this.db.insert(post).values(newPost).returning();
         return allPosts[0];
     }
 
     async getPost(id: string) {
         this.logger.info('Fetching post');
+        this.postHogService.captureEvent(`post-${id}`, 'fetched');
         const fetchedpost = await this.db.select().from(post).where(eq(post.id, id));
         return fetchedpost[0];
     }
 
     async updatePost(id: string, newPost: NewPost) {
         this.logger.info('Updating post');
+        this.postHogService.captureEvent(`post-${id}`, 'updated');
         const updatedPost = await this.db.update(post).set(newPost).where(eq(post.id, id)).returning();
         return updatedPost[0];
     }
 
     async deletePost(id: string) {
         this.logger.info('Deleting post');
+        this.postHogService.captureEvent(`post-${id}`, 'deleted');
         const deletedPost = await this.db.delete(post).where(eq(post.id, id)).returning();
         return deletedPost[0];
     }
